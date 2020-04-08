bf30f58d219d64d6d6cf4fca7420e218c9520fea
jordanrob709 <jordanrob709@gmail.com>
2020-04-09 02:52:48 +0300

    initial commit

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,19 @@
 def estimator(data):
-  return data
+    return data
+
+
+def covid19ImpactEstimator(reportedCases):
+    output = {
+        'impact': {
+            'currentlyInfected': reportedCases * 10,
+            'infectionsByRequestedTime': (reportedCases * 10) * (2 ** 19)
+        },
+
+        'severeImpact': {
+            'currentlyInfected': reportedCases * 50,
+            'infectionsByRequestedTime': (reportedCases * 10) * (2 ** 19)
+        }
+
+    }
+
+    return output
