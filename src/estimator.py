bb5746696526a331044c63abf3fe8297953fb9e7
jordanrob709 <jordanrob709@gmail.com>
2020-04-11 22:00:27 +0300

    part 1 fix

--- src/estimator.py
+++ src/estimator.py
@@ -1,26 +1,55 @@
 
 
-def estimator(data):
 
-    _data = data
+input_data = {
+        'region': {
+            'name': "Africa",
+            'avgAge': 19.7,
+            'avgDailyIncomeInUSD': 5,
+            'avgDailyIncomePopulation': 0.71
+        },
+        'periodType': "days",
+        'timeToElapse': 58,
+        'reportedCases': 674,
+        'population': 66622705,
+        'totalHospitalBeds': 1380614
+}
+
+def estimator(**input_data):
+
+
+    def convert_2_days():
+        if input_data['periodType'] == 'days':
+            converted_days = input_data['timeToElapse']
+
+        elif input_data['periodType'] == 'weeks':
+            converted_days = input_data['timeToElapse'] * 7
+
+        elif input_data['periodType'] == 'weeks':
+            converted_days = input_data['timeToElapse'] * 30
+
+        return converted_days    
+
 
     return {
-        'data': _data,
+        'data': input_data,
 
         'impact': {
-            'currentlyInfected': _data['reportedCases'] * 10,
-            'infectionsByRequestedTime': (_data['reportedCases'] * 10) * (2 ** 19)
+            'currentlyInfected': input_data['reportedCases'] * 10,
+            'infectionsByRequestedTime': (input_data['reportedCases'] * 10) * (2 ** ( convert_2_days() / 3 ))
         },
 
         'severeImpact': {
-            'currentlyInfected': _data['reportedCases'] * 50,
-            'infectionsByRequestedTime': (_data['reportedCases'] * 10) * (2 ** 19)
+            'currentlyInfected': input_data['reportedCases'] * 50,
+            'infectionsByRequestedTime': (input_data['reportedCases'] * 10) * (2 ** ( convert_2_days() / 3))
         }
 
     }
 
 
 '''
-cc = {'sa': 4}
-print(cc['sa'])
+
+a = 34
+b = 34 % 3
+print(b)
 '''
